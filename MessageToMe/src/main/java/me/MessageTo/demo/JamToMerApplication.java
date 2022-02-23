7e9b00ba05796bb091c6c4d1d955e3808203d2d9
superkingyj@naver.com <dbwlscjswosla13>
2022-02-23 20:23:51 +0900

    feat: setting project files

---
+++ MessageToMe/src/main/java/me/MessageTo/demo/JamToMerApplication.java
@@ -0,0 +1,13 @@
+package me.MessageTo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamToMerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamToMerApplication.class, args);
+	}
+
+}
---
+++ MessageToMe/src/test/java/me/MessageTo/demo/JamToMerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.MessageTo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamToMerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
